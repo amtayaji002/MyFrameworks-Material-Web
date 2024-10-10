851311c364ead6a585f8b40cc40087fe30eab2cb
amtayaji002 <tyagiamit1975@yahoo.com>
2024-10-10 22:35:14 +0530

    Initial working commit

---
+++ at-samples-00001/at-00001-omdbapi-basic/src/App.tsx
@@ -0,0 +1,23 @@
+import React from 'react';
+import './App.css';
+
+import '@fontsource/roboto/300.css';
+import '@fontsource/roboto/400.css';
+import '@fontsource/roboto/500.css';
+import '@fontsource/roboto/700.css';
+
+import ButtonUsage from './Components/ButtonUsage';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <p>
+          Edit <code>src/App.tsx</code> and save to reload.
+        </p>
+      </header>
+    </div>
+  );
+}
+
+export default App;
