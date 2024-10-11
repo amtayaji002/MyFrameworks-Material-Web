c946dee68fab676b83aa0beb153b804ce3f50b1d
amtayaji002 <tyagiamit1975@yahoo.com>
2024-10-11 14:08:12 +0530

    Initial working template for React Books to stand on

--- at-samples-00001/at-00001-omdbapi-basic/src/App.tsx
+++ at-samples-00001/at-00001-omdbapi-basic/src/App.tsx
@@ -1,23 +1,54 @@
 import React from 'react';
 import './App.css';
 
+import Container from '@mui/material/Container';
+import Typography from '@mui/material/Typography';
+import Box from '@mui/material/Box';
+import Link from '@mui/material/Link';
+
+
 import '@fontsource/roboto/300.css';
 import '@fontsource/roboto/400.css';
 import '@fontsource/roboto/500.css';
 import '@fontsource/roboto/700.css';
 
 import ButtonUsage from './Components/ButtonUsage';
+import ProTip from './ProTip';
+
+
+function Copyright() {
+  return (
+    <Typography
+      variant="body2"
+      align="center"
+      sx={{
+        color: 'text.secondary',
+      }}
+    >
+      {'Copyright © '}
+      <Link color="inherit" href="https://mui.com/">
+        Your Website
+      </Link>{' '}
+      {new Date().getFullYear()}.
+    </Typography>
+  );
+}
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <ButtonUsage ></ButtonUsage>
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload. <br />
-          
-        </p>
-      </header>
+      <main>
+        <ButtonUsage></ButtonUsage>
+      </main>
+
+      <footer>
+        <Container maxWidth="sm">
+          <Box sx={{ my: 4 }}>
+            <ProTip />
+            <Copyright />
+          </Box>
+        </Container>
+      </footer>
     </div>
   );
 }
--- at-samples-00001/at-00001-omdbapi-basic/src/index.tsx
+++ at-samples-00001/at-00001-omdbapi-basic/src/index.tsx
@@ -4,13 +4,26 @@ import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 
+import CssBaseline from '@mui/material/CssBaseline';
+import { ThemeProvider } from '@mui/material/styles';
+import theme from './theme';
+
+
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
 );
+// root.render(
+//   <React.StrictMode>
+//     <App />
+//   </React.StrictMode>
+// );
+
 root.render(
-  <React.StrictMode>
+  <ThemeProvider theme={theme}>
+    {/* CssBaseline kickstart an elegant, consistent, and simple baseline to build upon. */}
+    <CssBaseline />
     <App />
-  </React.StrictMode>
+  </ThemeProvider>,
 );
 
 // If you want to start measuring performance in your app, pass a function
